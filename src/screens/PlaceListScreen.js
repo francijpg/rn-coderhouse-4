c469c32c611908646b13fc47b3f61e8dd9aea489
Francisco PG <hellofrancijpg@gmail.com>
2022-07-18 20:56:40 -0400

    Update PlaceListScreen.js

--- src/screens/PlaceListScreen.js
+++ src/screens/PlaceListScreen.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, StyleSheet, Text, FlatList } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import PlaceItem from "../components/PlaceItem";
+import { loadPlaces } from "../store/place.slice";
 
 const styles = StyleSheet.create({
   container: {
@@ -11,26 +12,31 @@ const styles = StyleSheet.create({
     marginVertical: 20,
     alignItems: "center",
     justifyContent: "center",
-  }
+  },
 });
 
 const PlaceListScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
   const places = useSelector((state) => state.place.places);
+
+  useEffect(() => {
+    dispatch(loadPlaces());
+  }, []);
 
   const onSelectPlace = (id) => {
     navigation.navigate("PlaceDetail", { placeId: id });
-  }
+  };
   const renderItem = ({ item }) => (
     <PlaceItem {...item} onSelect={onSelectPlace} />
-  )
+  );
 
   const ListEmptyComponent = () => (
     <View style={styles.emptyContainer}>
       <Text>No hay lugares disponibles</Text>
     </View>
-  )
+  );
   return (
-    <FlatList 
+    <FlatList
       style={styles.container}
       data={places}
       keyExtractor={(item) => item.id.toString()}
