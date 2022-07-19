a22288c8acadcb66a1ce2bf305c18fee2e5c29b0
Francisco PG <hellofrancijpg@gmail.com>
2022-07-18 20:56:43 -0400

    Update MapPreview.js

--- src/components/MapPreview.js
+++ src/components/MapPreview.js
@@ -3,28 +3,28 @@ import { View, Image, StyleSheet } from "react-native";
 import { URL_MAPS } from "../utils/maps";
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    mapImage: {
-        width: "100%",
-        height: "100%",
-    }
-})
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  mapImage: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 const MapPreview = ({ children, location, style }) => {
-    const { lat, lng } = location || {};
-    const mapPreviewUrl = location ?  URL_MAPS(lat, lng) : '';
-    return (
-        <View style={{...styles.container, ...style}}>
-            {location ? (
-                <Image style={styles.mapImage} source={{ uri: mapPreviewUrl}} />
-            ) : (
-                children
-            )}
-        </View>
-    )
-}
+  const { lat, lng } = location || {};
+  const mapPreviewUrl = location ? URL_MAPS(lat, lng) : "";
+  return (
+    <View style={{ ...styles.container, ...style }}>
+      {location ? (
+        <Image style={styles.mapImage} source={{ uri: mapPreviewUrl }} />
+      ) : (
+        children
+      )}
+    </View>
+  );
+};
 
 export default MapPreview;
